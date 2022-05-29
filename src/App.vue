ca5110e8fe958496112acf580ec24e9d5da680b6
Germinal Camps <gcampsbarjau@gmail.com>
2022-05-29 22:29:26 +0200

    scroll

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,12 @@
 
 <script>
 // 1: importar el component
-import HelloWorld from "./components/HelloWorld.vue";
-import Counter from "./components/Counter.vue";
-import Bank from "./components/Bank.vue";
 import Scroll from "./components/Scroll.vue";
 
 export default {
   // 2: registrar el component
   components: {
-    HelloWorld,
-    Counter,
     Scroll,
-  },
-  data() {
-    return {
-      max1: 10,
-    };
   },
 };
 </script>
