ea62b77998f29c472622984f270c292ede290416
Germinal Camps <gcampsbarjau@gmail.com>
2022-05-29 22:28:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="">
+    <!-- 3. utilitzar el component al template -->
+
+    <Scroll />
+  </div>
+</template>
+
+<script>
+// 1: importar el component
+import HelloWorld from "./components/HelloWorld.vue";
+import Counter from "./components/Counter.vue";
+import Bank from "./components/Bank.vue";
+import Scroll from "./components/Scroll.vue";
+
+export default {
+  // 2: registrar el component
+  components: {
+    HelloWorld,
+    Counter,
+    Scroll,
+  },
+  data() {
+    return {
+      max1: 10,
+    };
+  },
+};
+</script>
---
+++ src/components/Scroll.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-for="n in 50" :key="n">
+    <img
+      :src="`https://picsum.photos/id/${n * 10}/300`"
+      alt=""
+      class="w-full"
+    />
+  </div>
+
+  <div class="fixed top-4 w-screen px-4">
+    <div class="bg-white h-4 rounded-full w-full">
+      <div
+        :style="{ width: `${width}%` }"
+        class="bg-sky-500 h-full rounded-full transition-all"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const width = ref(100);
+
+setInterval(() => {
+  if (width.value > 0) {
+    width.value -= 1;
+  }
+}, 100);
+
+window.addEventListener("scroll", () => {
+  if (width.value < 100) {
+    width.value += 0.3;
+  }
+});
+</script>
